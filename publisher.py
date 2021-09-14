cc709f303632412aa652fff25b8eba05cf99675a
tkangsilalai <54450088+tkangsilalai@users.noreply.github.com>
2021-09-14 14:02:07 +0700

    fix: some errors

--- publisher.py
+++ publisher.py
@@ -3,10 +3,13 @@ import paho.mqtt.client as mqtt
 import time
 import random
 import json
+from dotenv import load_dotenv
 from dto import Payload
 
+load_dotenv(os.path.join(os.path.dirname('./'), '.env'))
+
 client = mqtt.Client()
-client.connect(os.environ.get("BROKER_HOST"), os.environ.get("BROKER_PORT"), 60)
+client.connect("54.169.177.53", 1883, 60)
 
 for i in range(100) :
     obj = Payload(str(random.randint(0,11)), 1 if random.randint(0,100) > 30 else 0)
