908e0eb94877ee500ebfcafcac77a7c6e00d1f81
passawityakul <p.yakul@icloud.com>
2021-10-03 16:20:45 +0700

    demo

--- mock_all_sensors.py
+++ mock_all_sensors.py
@@ -10,10 +10,9 @@ load_dotenv(os.path.join(os.path.dirname('./'), '.env'))
 client = mqtt.Client()
 client.connect(os.environ.get("BROKER_HOST"), int(os.environ.get("BROKER_PORT")), 60)
 
-for i in range(1000) :
+
+while True:
     obj = Payload(str(random.randint(0, 44)), 1 if random.randint(0, 100) > 30 else 0)
     client.publish(str(os.environ.get("ACTION_TOPIC")), obj.serialize())
     time.sleep(1)
     print(obj.serialize())
-    
-client.loop(2)
--- sensor.py
+++ sensor.py
@@ -10,11 +10,16 @@ from mock_sensor import get_action
 SENSOR_ID = 1
 
 load_dotenv(os.path.join(os.path.dirname('./'), '.env'))
+
+#Connect to mqtt client
 client = mqtt.Client()
 client.connect(os.environ.get("BROKER_HOST"), int(os.environ.get("BROKER_PORT")), 60)
 
 while True:
+    #Get action from the sensor
     action = get_action()
+    
+    #If action is not None, publish to the broker server
     if action != None:
         #Construct payload for sending over network 
         payload = Payload(SENSOR_ID, action)
@@ -22,6 +27,9 @@ while True:
         #Publishing message
         message = payload.serialize()
         client.publish(os.environ.get("ACTION_TOPIC"), message)
+        
         print(message)
+    
+    #Interval simulating as real world data
     time.sleep(1)
     
